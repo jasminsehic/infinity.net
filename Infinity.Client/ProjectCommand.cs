89687d94640f1f42a38d18ffca1cec78c00e8755
Edward Thomson <ethomson@microsoft.com>
2015-09-11 11:50:35 -0400

    Infinity.Client: Add ProjectCommand for debugging

---
+++ Infinity.Client/ProjectCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Infinity;
+using Infinity.Clients;
+using Infinity.Models;
+using Infinity.Util;
+
+namespace Infinity.Client
+{
+    public class ProjectCommand
+    {
+        public ProjectCommand(TfsClient client)
+        {
+            Assert.NotNull(client, "client");
+
+            Client = client;
+        }
+
+        private TfsClient Client { get; set; }
+
+        public int GetProjects(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                Console.Error.WriteLine("usage: {0} <url> Project.GetProjects", Program.ProgramName);
+                return 1;
+            }
+
+            IEnumerable<Project> projects = null;
+
+            Task.Run(async () =>
+            {
+                projects = await Client.Project.GetProjects();
+            }).Wait();
+
+            foreach (Project project in projects)
+            {
+                Console.WriteLine("Project {0}:", project.Id);
+                Model.Write(project);
+            }
+
+            return 0;
+        }
+    }
+}
